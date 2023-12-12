3de7d7731a937c38e22991db050e2157fc039d47
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-12 16:27:42 -0300

    Alterações nos cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,31 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+// Adicionar classe .zoom ao card de Pokémon
+pokemonList.querySelectorAll('.pokemon').forEach((pokemon) => {
+    pokemon.classList.add('zoom')
+})
+
+// Adicionar evento listener ao card de Pokémon para o evento 'mouseenter'
+pokemonList.querySelectorAll('.pokemon').forEach((pokemon) => {
+    pokemon.addEventListener('mouseenter', () => {
+    // Aplicar zoom ao card
+    pokemon.style.transform = 'scale(1.2)'
+
+    // Adicionar informações e habilidades do Pokémon
+    const infoDiv = document.createElement('div')
+    infoDiv.classList.add('info')
+    infoDiv.innerHTML = `
+      <h3>${card.querySelector('.name').innerHTML}</h3>
+      <p>Número: ${card.querySelector('.number').innerHTML}</p>
+      <p>Tipos: ${card.querySelector('.types li').innerHTML}</p>
+      <p>Habilidades: ${card.querySelector('.abilities li').innerHTML}</p>
+    `
+    card.appendChild(infoDiv)
+  })
+})
+
+// Aplicar zoom ao card com transição
+pokemon.style.transform = 'scale(1.2)'
+pokemon.style.transition = 'transform 0.2s ease-in-out'
